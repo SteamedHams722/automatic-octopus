e71d6e0b8135c5f6908c598bca482806d7461a25
Steamed Hams <steamedhams722@protonmail.com>
2020-11-28 15:37:09 -0600

    Add authentication test code
    Why
    An authentication flow needs to be set-up so this app can access the
    user data.
    
    How
    * Add the basic authentication flow instructions from the Spotipy website.
    It's not what I'll end up using, but it's good to have a record of what works.
    
    Note
    This requires user input each time to authorize the transaction so I need
    to fix that so it's more automated.

--- core/sound.py
+++ core/sound.py
@@ -1,2 +1,14 @@
-# This will store the initial API code. More files may be needed depending on
-# how complex this gets
+# Import necessary libraries
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+#Set-up scope
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+results = sp.current_user_saved_tracks()
+
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
