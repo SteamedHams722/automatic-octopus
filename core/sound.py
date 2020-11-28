3ec8f0f30b4b6273e215f7e98110f2437e9a175a
Steamed Hams <steamedhams722@protonmail.com>
2020-11-28 15:57:25 -0600

    Add recently played api call
    Why
    The main goal is to use the recently played tracks to make predictions.
    In sound.py, that scope and call should be used.
    
    How
    * Add the necessary scope and API call for getting recently played tracks
    
    Note
    This is still in the testing phase since I need to determine the time frame
    to use. I also need to figure out how to set-up automatic authentication so
    I can run it nightly.

--- core/sound.py
+++ core/sound.py
@@ -1,14 +1,22 @@
 # Import necessary libraries
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import simplejson as json
 
 #Set-up scope
-scope = "user-library-read"
+scope = 'user-read-recently-played'
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-results = sp.current_user_saved_tracks()
+results = sp.current_user_recently_played(limit=50, after=None, before=None)
 
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+json_res = json.dumps(results, sort_keys=True, indent=4 * ' ')
+
+#print(json_res)
+
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+with open('test.txt', 'w') as outfile:
+    json.dump(json_res, outfile)
