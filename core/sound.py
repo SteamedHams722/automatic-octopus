682091e1fd6c1dc7f08902fd50dc60ed58be55cf
Steamed Hams <steamedhams722@protonmail.com>
2020-11-29 11:45:26 -0600

    Fix issues with json output
    Why
    The json output was full of unnecessary special characters (ex. newline)
    which made it hard to read and difficult to query through SQL.
    
    How
    * Remove the json_res variable and the json.dumps. This was causing part
    of the formatting issue.
    * Add encoding = 'utf-8' to the test file open code since this prevents
    errors with some of the characters from the API pull.
    * Add the results variable to the json.dump function. This is all that's needed
    to load the json data.
    
    Note
    I also removed some of the logic for returning the list of top songs since
    it's irrelevant.

--- core/sound.py
+++ core/sound.py
@@ -10,13 +10,6 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 results = sp.current_user_recently_played(limit=50, after=None, before=None)
 
-json_res = json.dumps(results, sort_keys=True, indent=4 * ' ')
-
-#print(json_res)
-
-# for idx, item in enumerate(results['items']):
-#     track = item['track']
-#     print(idx, track['artists'][0]['name'], " – ", track['name'])
-
-with open('test.txt', 'w') as outfile:
-    json.dump(json_res, outfile)
+# Create the json file to pull the stored data
+with open('test.json', 'w',  encoding='utf-8') as outfile: #Need to add encoding to prevent error
+    json.dump(results, outfile, sort_keys=True, ensure_ascii=False, indent=2)
