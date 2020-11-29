01085ed2d7fda643ca330f52090467c2794bd08d
Steamed Hams <steamedhams722@protonmail.com>
2020-11-29 14:41:19 -0600

    Create list for storing track IDs
    Why
    Since I'll be bringing in the recently played tracks for analysis, I'll
    also want to tie in the audio analysis and audio features. This requires
    a list of all the IDs for matching.
    
    How
    * Create a tracks list object
    * For every dictionary in the results['items'] list in the API object,
    append the ID to the tracks list

--- core/sound.py
+++ core/sound.py
@@ -1,15 +1,21 @@
 # Import necessary libraries
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import simplejson as json
+import json
 
-#Set-up scope
+# Set-up scope
 scope = 'user-read-recently-played'
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
+# Pull in API data
+# No need to worry about before/after since that is better handled within SQL
 results = sp.current_user_recently_played(limit=50, after=None, before=None)
 
 # Create the json file to pull the stored data
-with open('test.json', 'w',  encoding='utf-8') as outfile: #Need to add encoding to prevent error
-    json.dump(results, outfile, sort_keys=True, ensure_ascii=False, indent=2)
+# with open('test.json', 'w',  encoding='utf-8') as outfile: #Need to add encoding to prevent error
+#     json.dump(results, outfile, sort_keys=True, ensure_ascii=False, indent=2)
+
+tracks = []
+for item in results['items']:
+    tracks.append(item['track']['id'])
+
