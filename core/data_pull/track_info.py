7524f155ac3b12b3c46047c7aebd5d0b05414588
Steamed Hams <steamedhams722@protonmail.com>
2020-11-29 16:34:14 -0600

    Add track_ids function
    Why
    There needs to be a quick way to pull in all of the track IDs for the
    recently played tracks.
    
    How
    * Add a function to track_info.py called track_ids
    * Set the results variable to the output of the recently_played function
    * Iterate through each "item" dictionary in the items list to find the
    track ID.
    * Append each ID to the tracks list
    * De-duplicate the IDs in the list by using the dict.fromkeys function.
    This is necessary to prevent unnecessary API calls in downstream functions.

--- core/data_pull/track_info.py
+++ core/data_pull/track_info.py
@@ -6,7 +6,7 @@ from spotipy.oauth2 import SpotifyOAuth
 def recently_played():
     """Return the recently played tracks API data"""
 
-    # Set-up scope
+    # Set-up authorization scope. This is needed since it accesses user data
     scope = 'user-read-recently-played'
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
@@ -15,6 +15,23 @@ def recently_played():
     results = sp.current_user_recently_played(limit=50, after=None, before=None)
 
     return results
+
+def track_ids():
+    """Gets the track IDs for the recently played tracks"""
+
+    #Get the json object from the previous function
+    results = recently_played()
+
+    #Loop through each dictionary in the items list to get the track IDs
+    tracks = []
+    for item in results['items']:
+        tracks.append(item['track']['id'])
+    
+    # De-duplicate the IDs in the list. This prevents downstream functions from
+    # hitting the 100 ID limit
+    tracks = list(dict.fromkeys(tracks))
+
+    return tracks
 
 
 
