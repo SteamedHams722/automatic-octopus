117678e0484e551d28c90012620d518dfeb24ed2
Steamed Hams <steamedhams722@protonmail.com>
2020-12-05 19:40:33 -0600

    remove the audio analysis function
    Why
    The results returned for audio analysis require a for loop and could hit
    the API limit. In addition, the data here is not valuable enough to
    warrant the risk and the time spent looping through each track.
    
    How
    * Remove the analysis function from track_metrics.

--- core/data_pull/track_metrics.py
+++ core/data_pull/track_metrics.py
@@ -17,19 +17,3 @@ def features():
     track_features = sp.audio_features(tracks)
 
     return track_features
-
-def analysis():
-    """Return detailed audio analysis of each recently played track. Since a for loop
-    is required, this will cause ~50 calls to the API so it's probably best not
-    to use this function unless the data proves to be valuable"""
-
-    #A list of IDs can't be used so the audio analysis data needs to be iterated 
-    # for each ID, create an ID/analysis dictionary, and add it to a list of all 
-    # the analysis dictionaries
-    analyses = []
-    for track_id in tracks:
-        track_analysis = sp.audio_analysis(track_id)
-        analysis_dict = {track_id: track_analysis}
-        analyses.append(analysis_dict)
-    
-    return analyses
