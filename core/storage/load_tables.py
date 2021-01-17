8fc7e2ae538673cd27cdb943aa7cc932c3cf7ade
Steamed Hams <steamedhams722@protonmail.com>
2021-01-16 21:27:35 -0600

    Fix the load_tables script
    Why
    The script wasn't working so there were several changes that needed to be added
    
    How
    - Remove unnecessary libraries
    - Add the datetime and logging libraries since those are needed for
    logging the execution status
    - Comment out the track function imports since those still need to be fixed
    - Add additional variables for the raw db and the spotify schema
    - Add temporary info_data and feature_data string variables to test the insert
    - Add a dictionary for the table_data. This way, any new tables can be easily added to
    the dictionary and looped through without having to add an if/else statement
    to check the table to load
    - Add logging config.
    - Add code under the top level try block that says that the job is initiated. This
    includes a timestamp field to help with tracking.
    - Use with cursor before the subset of try blocks. This way it can be used across
    all the sql executions. The with makes sure to close it once it's complete.
    - Add logic to create the schema if it doesn't exist. A try/except/else
    is used with logging to make sure common errors are handled and tracked.
    - Change the for loop to iterate through the dictionary so both the table (key) and
    the data (value) can be used.
    - Remove the if/elif code since the dictionary approach makes it obsolete
    - Add similar try/except and logging logic for the create table and insert
    table logic.
    - Add logging to the finally block since it's good to know that the data load
    is complete and the connection closed.

--- core/storage/load_tables.py
+++ core/storage/load_tables.py
@@ -1,42 +1,83 @@
+#pylint: disable=no-member
 '''Use this script to load the track_info and track_metrics tables in the raw 
 database'''
 
 # Import libraries
-import simplejson as json
 import sys
 import os
+import logging
+from datetime import datetime
 user_home = os.path.expanduser("~").replace(os.sep,'/')
 sys.path.append(user_home + r"/automaton/core/data_pull")
-from tracks import track_features, recently_played #pylint: disable=import-error
+#from tracks import track_features, recently_played#pylint: disable=import-error
 from postgres_connections import pg_conn
-from psycopg2 import ProgrammingError, sql
+from psycopg2 import ProgrammingError, errors
 
 # Establish the necessary variables
-tables = ["track_info", "track_metrics"]
-_, info = recently_played()
-features = track_features()
+db = "raw"
+schema = "spotify"
+info_data = 'Homer' #Temp until I can get the spotify data working
+feature_data = 'Burnsie' #Temp until I can get the spotify data working
+table_data = {"track_info": info_data, "track_metrics": feature_data}
+
+# Set-up logging
+logging.basicConfig(filename='load.log', filemode='a', level=logging.DEBUG)
 
 # Open a cursor for the insert statements
 conn = pg_conn()
 cursor = conn.cursor()
 
 try: #Top level try block for closing connection and cursor
-    try:
-        # Loop through each table in the list to insert the data. Also creates 
-        # the table if it doesn't exist.
-       for table in tables:
-            cursor.execute(
-                f'''CREATE TABLE IF NOT EXISTS spotify.{table} (src jsonb);'''
-            )
-            #TODO: Find a better way to do this so a hardcoded if is not allowed. ALso,
-            # investigate why using the parameterization causes a syntax error
-            if table == "track_info":
-                cursor.execute('''INSERT INTO spotify.track_info (src) VALUES (%s)''', (info))
-            elif table == "track_metrics":
-                cursor.execute('''INSERT INTO spotify.track_metrics (src) VALUES (%s)''', (features))
+    timestamp = datetime.utcnow().replace(microsecond=0)
+    message = f"{timestamp} Postgres data load initiated."
+    logging.exception(message)
+
+    # Open up the SQL cursor so the commands can be executed
+    with cursor:
+
+        #Create the schema if it doesn't exist
+        try:
+            create_schema = "create schema if not exists {0};".format(schema)
+            cursor.execute(create_schema)
             conn.commit()
-    except ProgrammingError as err:
-        print(f"Unable to insert data into postgres. Message: {err}")
+        except (ProgrammingError, errors.InFailedSqlTransaction) as err:
+            timestamp = datetime.utcnow().replace(microsecond=0)
+            error = f"{timestamp} ERROR: There was an issue creating the {schema} schema. Message: {err}"
+            logging.exception(message)
+        else:
+            timestamp = datetime.utcnow().replace(microsecond=0)
+            message = f"{timestamp} SUCCESS: The {schema} schema is in the {db} database."
+            logging.exception(message)
+
+            # Loop through each dictionary entry to insert the data. Also creates 
+            # the table if it doesn't exist.
+        for table, data in table_data.items():
+            try:
+                create_table = "create table if not exists {0}.{1}.{2} (src varchar(50));".format(db, schema, table)
+                cursor.execute(create_table)
+                conn.commit()
+            except (ProgrammingError, errors.InFailedSqlTransaction) as err:
+                timestamp = datetime.utcnow().replace(microsecond=0)
+                error = f"{timestamp} ERROR: There was an issue creating the {table} table. Message: {err}"
+                logging.exception(message)
+            else:
+                timestamp = datetime.utcnow().replace(microsecond=0)
+                message = f"{timestamp} SUCCESS: The {table} table is in the {schema} schema."
+                logging.exception(message)
+            
+            try:
+                insert_data = "insert into {0}.{1}.{2} values ('{3}');".format(db, schema, table, data)
+                cursor.execute(insert_data)
+                conn.commit()
+            except (ProgrammingError, errors.InFailedSqlTransaction) as err:
+                message = f"Unable to insert data into the {table} table. Message: {err}"
+                logging.exception(message)
+            else:
+                timestamp = datetime.utcnow().replace(microsecond=0)
+                message = f"{timestamp} SUCCESS: Data was inserted into the {table} table."
+                logging.exception(message)
 finally:
-    cursor.close()
     conn.close()
+    timestamp = datetime.utcnow().replace(microsecond=0)
+    message = f"{timestamp} Postgres data load completed. Connection closed."
+    logging.exception(message)
