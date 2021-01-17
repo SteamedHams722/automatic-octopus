77758b5fa13493440386ba0eea268e3df97b87a7
Steamed Hams <steamedhams722@protonmail.com>
2021-01-17 13:22:16 -0600

    Load the spotify api data
    Why
    Now that the load tables script is able to load static data, it's time to adjust
    it so it can load the dynamic api data from spotify.
    
    How
    - Uncomment the import for the tracks modules
    - Assign the results of recently_played and track_features to variables
    - Replace single quotes in those variables with '' since that's the simplest way to
    upload single quotes to postgres.
    - Add the created_on timestamp field to the tables. This is necessary for determining
    which records to use since we only want to use the most recent api pull for predictions.
    - Use utcnow as the value for the timestamp in the insert code

--- core/storage/load_tables.py
+++ core/storage/load_tables.py
@@ -9,16 +9,18 @@ import logging
 from datetime import datetime
 user_home = os.path.expanduser("~").replace(os.sep,'/')
 sys.path.append(user_home + r"/automaton/core/data_pull")
-#from tracks import track_features, recently_played#pylint: disable=import-error
+from tracks import track_features, recently_played#pylint: disable=import-error
 from postgres_connections import pg_conn
 from psycopg2 import ProgrammingError, errors
 
 # Establish the necessary variables
 db = "raw"
 schema = "spotify"
-info_data = r'{"Homer": {"Intelligence":"Dumb", "Weight":"Fat", "Age":"Young"}}' #Temp until I can get the spotify data working
-feature_data = r'{"Homer": {"Jobs":["Astronaut", "Garbage Guy", "Bodyguard"]}}'
-table_data = {"track_info": info_data, "track_metrics": feature_data}
+_, info_json = recently_played()
+info_data = info_json.replace("'","''") #Have to escape single quotes in a postgres friendly way
+features_json = track_features()
+features_data = features_json.replace("'","''")
+table_data = {"track_info": info_data, "track_features": features_data}
 
 # Set-up logging
 logging.basicConfig(filename='load.log', filemode='a', level=logging.DEBUG)
@@ -40,7 +42,7 @@ try: #Top level try block for closing connection and cursor
             create_schema = "create schema if not exists {0};".format(schema)
             cursor.execute(create_schema)
             conn.commit()
-        except (ProgrammingError, errors.InFailedSqlTransaction, errors.SyntaxEror) as err:
+        except (ProgrammingError, errors.InFailedSqlTransaction, errors.SyntaxError) as err:
             timestamp = datetime.utcnow().replace(microsecond=0)
             error = f"{timestamp} ERROR: There was an issue creating the {schema} schema. Message: {err}"
             logging.exception(message)
@@ -55,11 +57,12 @@ try: #Top level try block for closing connection and cursor
             try:
                 create_table = '''create table if not exists {0}.{1}.{2} ( 
                     id serial not null primary key,
-                    src json not null
+                    src json not null,
+                    created_on timestamp not null
                     );'''.format(db, schema, table)
                 cursor.execute(create_table)
                 conn.commit()
-            except (ProgrammingError, errors.InFailedSqlTransaction, errors.SyntaxEror) as err:
+            except (ProgrammingError, errors.InFailedSqlTransaction, errors.SyntaxError) as err:
                 timestamp = datetime.utcnow().replace(microsecond=0)
                 error = f"{timestamp} ERROR: There was an issue creating the {table} table. Message: {err}"
                 logging.exception(message)
@@ -69,11 +72,12 @@ try: #Top level try block for closing connection and cursor
                 logging.info(message)
             
             try:
-                insert_data = '''insert into {0}.{1}.{2} (src) 
-                    values ('{3}');'''.format(db, schema, table, data)
+                timestamp = datetime.utcnow().replace(microsecond=0)
+                insert_data = '''insert into {0}.{1}.{2} (src, created_on) 
+                    values ('{3}','{4}');'''.format(db, schema, table, data, timestamp)
                 cursor.execute(insert_data)
                 conn.commit()
-            except (ProgrammingError, errors.InFailedSqlTransaction, errors.SyntaxEror) as err:
+            except (ProgrammingError, errors.InFailedSqlTransaction, errors.SyntaxError) as err:
                 message = f"Unable to insert data into the {table} table. Message: {err}"
                 logging.exception(message)
             else:
