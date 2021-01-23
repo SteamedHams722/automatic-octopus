8dc889d9c8df748d73f3572225018ead430ba116
Steamed Hams <steamedhams722@protonmail.com>
2021-01-23 17:52:19 -0600

    Test out token authentication
    Why
    In order to automate the postgres pull, I need a way to keep the token
    refreshed so it's not required to log-in between sessions. This commit
    represents the initial attempt at identifying the token.
    
    How
    - Clean-up the oauth function. There were some unnecessary
    pieces of code that could be removed
    - Add the initial token info to the oauth function so that can get returned too
    
    Note
    This isn't working yet, but it's a start and should be committed.

--- core/data_pull/connections.py
+++ core/data_pull/connections.py
@@ -3,20 +3,37 @@
 
 # Import necessary libraries
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.exceptions import SpotifyException #Will need this for exception handling
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 
-# Set up OAuth function
-def oauth(scope, auth_manager=SpotifyOAuth):
-    '''Return the OAuth information needed to access user data'''
-    oauth_conn = spotipy.Spotify(auth_manager=auth_manager(scope=scope))
-
-    return oauth_conn
-
-def client(auth_manager=SpotifyClientCredentials):
+#Set up client credentials needed for basic info
+def client():
     '''Return the client credentials needed to access non-user data'''
-    client_conn = spotipy.Spotify(auth_manager=auth_manager())
+    client_conn = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 
     return client_conn
 
+# Set up OAuth functions for user-specific info
+def oauth(scope):
+    '''Return the OAuth information needed to access user data and refresh the
+    token as needed.'''
+    #Get the base authentication connection
+    sp_oauth = SpotifyOAuth(scope=scope)
+    oauth_conn = spotipy.Spotify(auth_manager=sp_oauth)
+
+    #Set-up a token refresh so users don't have to log in constantly
+    token_info = sp_oauth.get_cached_token() 
+    if not token_info:
+        auth_url = sp_oauth.get_authorize_url()
+        print(auth_url)
+        response = input('Paste the above link into your browser, then paste the redirect url here: ')
+
+        code = sp_oauth.parse_response_code(response)
+        token_info = sp_oauth.get_access_token(code)
+
+        token = token_info['access_token']
+
+    return oauth_conn, token
+
+conn, token = oauth(scope='user-read-recently-played')
+
+print(token)
