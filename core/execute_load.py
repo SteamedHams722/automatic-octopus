25b7a9ffc7f8f16bd61f181230ef39acf796fd85
Steamed Hams <steamedhams722@protonmail.com>
2021-01-31 13:59:55 -0600

    Add track analysis to execute script.
    Why
    Since the track analysis is now accessible, it needs to be pulled into
    postgres on the same schedule as the other sources.
    
    How
    - Import the sleep function. It will be needed for adding a delay befor the
    track analysis function runs.
    - Create a dictionary of the success values for each functions. This set-up
    will be increasingly important as more data loads are added.
    - Iterate through each key/value in the success dictionary so the communicado
    function can run for each table group and success value to determine if a
    message should be sent and what the contents of the message are.
    -

--- core/execute_load.py
+++ core/execute_load.py
@@ -7,11 +7,12 @@
 import sys
 import os
 from datetime import datetime
+from time import sleep
 import logging
 user_home = os.path.expanduser("~").replace(os.sep,'/')
 sys.path.append(user_home + r"/automatic-octopus/core/storage")
 sys.path.append(user_home + r"/automatic-octopus/core/message")
-from load_tracks import tracks_to_pg
+from load_tracks import tracks_to_pg, analysis_to_pg
 from load_responses import responses_to_pg
 from transmit import communicado
 
@@ -24,13 +25,17 @@ sheet_name = os.getenv("response_sheet")
 # Load the tables and send a text message if it fails
 track_success = tracks_to_pg()
 response_success = responses_to_pg(sheet_name)
+sleep(60) #Making the app sleep before it executes the analysis function
+analysis_success = analysis_to_pg()
+success_dict = {'tracks': track_success, 'responses': response_success, 'analysis': analysis_success}
 
-if track_success is False or response_success is False:
-    communicado(False)
-    timestamp = datetime.utcnow().replace(microsecond=0)
-    message = f" {timestamp} Failure message sent. There was an issue when trying to load data"
-    logging.info(message)
-else:
+for key, val in success_dict.items():
+  if val: # A True value means the job succeeded.
+    #communicado(table_group=key, success=val) #This is only needed for testing.
     timestamp = datetime.utcnow().replace(microsecond=0)
     message = f" {timestamp} No message sent. Data load jobs succeeded."
+  else:
+    communicado(table_group=key, success=val)
+    timestamp = datetime.utcnow().replace(microsecond=0)
+    message = f" {timestamp} Failure message sent. There was an issue when trying to load data"
     logging.info(message)
