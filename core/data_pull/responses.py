67f71b75459607555bc21de81581ad9b74c247f7
Steamed Hams <steamedhams722@protonmail.com>
2021-03-23 07:59:29 -0500

    Fix json keyfile
    Why
    Previously,the responses script called a local that stored the secrets data.
    Since Heroku is being used for this app, it needs to be handled different.
    
    How
    - Use from_json_keyfile dict. This may work since the value in heroku is stored
    as a dictionary.

--- core/data_pull/responses.py
+++ core/data_pull/responses.py
@@ -15,7 +15,7 @@ logging.basicConfig(filename='execute.log', filemode='a', level='INFO')
 #Authorization credentials for the Google Sheet
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 secret_file = os.getenv('service_account')
-creds = ServiceAccountCredentials.from_json_keyfile_name(secret_file, scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(secret_file, scope)
 client = gspread.authorize(creds)
 
 #Create a function that pulls the latest responses
