ab35e4f1e160a59dcf6b6b65457f0034451a408d
Steamed Hams <steamedhams722@protonmail.com>
2021-03-27 16:58:18 -0500

    Add original procfile code back in
    This is just needed to get the dyno for running the app. It won't run
    24/7. It makes more sense to have this rather than try to have a dedicated
    python script that runs  the cache creation.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-'''Simple script to create the cache file needed for authentication'''
-import os
+'''This is just a placeholder app for establishing the dynos'''
 
-#Create or overwrite the cache file with the cache variable data
-#Eventually, this will have to be scaled for each user.
-with open('.cache', 'w') as f:
-    f.write(os.getenv('SPOTIPY_CACHE'))
+#Add this code here:
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Champions of Winning, Superb!'
+
+if __name__ == "__main__":
+    app.run(os.getenv('PORT'))
