3569241e1e642e82d35e3d56424345c8ead7454a
Steamed Hams <steamedhams722@protonmail.com>
2021-03-27 16:22:39 -0500

    Create the .cache file for spotipy
    Why
    Since Heroku clears out all temp files after each run, the spotipy cache
    file is being purged. As a result, the authentication flow prompts a new
    authorization each time.
    
    How
    - Add code to the app.py file that creates the cache file with the value
    from the SPOTIPY_CACHE variable.
    
    Note
    This is still unntested.

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 '''This is just a placeholder app for establishing the dynos'''
 
+#Add this code here:
 import os
 from flask import Flask
+
+# Create the Spotipy cache file if it doesn't exist
+if not os.path.exists('.cache'):
+    with open('.cache', 'w') as f:
+        f.write(os.getenv('SPOTIPY_CACHE'))
 
 app = Flask(__name__)
 
